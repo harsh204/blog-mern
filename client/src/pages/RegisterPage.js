1960bf7b48a1aab6a9a845fc704cfde6d700e862
harsh204 <76209778+harsh204@users.noreply.github.com>
2023-11-13 19:44:09 +0530

    initial commit

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom";
+
+export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect,setRedirect]=useState(false);
+
+    async function register(ev) {
+        ev.preventDefault();
+
+        const response = await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        if (response.status === 200) {
+            alert('Registration Successful')
+            setRedirect(true);
+        }
+        else {
+            alert('Registration Failed. Please check username.')
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/login'} />
+    }
+    return (
+        <form className="register" onSubmit={register}>
+            <h1>Register</h1>
+            <input type="text" placeholder="Username" value={username} onChange={ev => setUsername(ev.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange={ev => setPassword(ev.target.value)} />
+            <button>Register</button>
+        </form>
+    )
+}
